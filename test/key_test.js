b33257d8ed581e03f0f14fdf68d8f83ea522eac1
Hideyuki MORITA <emeitch@emeitch.com>
2017-06-20 02:05:06 +0900

    複数プロパティパターンのテストを追加

--- test/key_test.js
+++ test/key_test.js
@@ -5,6 +5,8 @@ import Key from '../src/key';
 
 describe("Key", () => {
   const prop1 = new UUID();
+  const prop2 = new UUID();
+  const prop3 = new UUID();
 
   let key;
   beforeEach(() => {
@@ -20,6 +22,16 @@ describe("Key", () => {
   describe("#path", () => {
     it("should return property id array", () => {
       assert.deepStrictEqual(key.path, [prop1]);
+    });
+
+    context("many props", () => {
+      beforeEach(() => {
+        key = new Key(prop1, prop2, prop3);
+      });
+
+      it("should return many property ids array", () => {
+        assert.deepStrictEqual(key.path, [prop1, prop2, prop3]);
+      });
     });
   });
 });
