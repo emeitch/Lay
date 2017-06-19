eb6f1e2ffa416e3938c2ecd5ba4318d403ba450c
Hideyuki MORITA <emeitch@emeitch.com>
2017-06-19 13:19:43 +0900

    値オブジェクトをうまくStoreで扱えるようにKeyを追加

---
+++ src/key.js
@@ -0,0 +1,5 @@
+export default class Key {
+  constructor(...props) {
+    this.props = props;
+  }
+}
---
+++ test/key_test.js
@@ -0,0 +1,25 @@
+import assert from 'assert';
+
+import UUID from '../src/uuid';
+import Key from '../src/key';
+
+describe("Key", () => {
+  const prop1 = new UUID();
+
+  let key;
+  beforeEach(() => {
+    key = new Key(prop1);
+  });
+
+  describe("constructor", () => {
+    it("should return key instance", () => {
+      assert(key.constructor === Key);
+    });
+  });
+
+  describe("#props", () => {
+    it("should return property id array", () => {
+      assert.deepStrictEqual(key.props, [prop1]);
+    });
+  });
+});
