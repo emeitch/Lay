749f416e0f6774beedd53302e952e52d545226db
Hideyuki MORITA <emeitch@emeitch.com>
2017-07-03 00:47:40 +0900

    テストの記載順序を変更

--- test/path_test.js
+++ test/path_test.js
@@ -5,16 +5,6 @@ import UUID from '../src/uuid';
 import { self } from '../src/self';
 
 describe("Path", () => {
-  describe("#toString", () => {
-    it("should return ids joined by slash", () => {
-      const id1 = new UUID();
-      const id2 = new UUID();
-      const id3 = new UUID();
-      const p = new Path(id1, id2, id3);
-      assert.deepStrictEqual(p.toString(),`${id1}/${id2}/${id3}`);
-    });
-  });
-
   describe("constructor ", () => {
     it("should accept self", () => {
       assert.doesNotThrow(() => new Path(self));
@@ -26,6 +16,16 @@ describe("Path", () => {
 
     it("should require keys are typed ID", () => {
       assert.throws(() => new Path(new UUID(), "strobj"), / is not a ID$/);
+    });
+  });
+
+  describe("#toString", () => {
+    it("should return ids joined by slash", () => {
+      const id1 = new UUID();
+      const id2 = new UUID();
+      const id3 = new UUID();
+      const p = new Path(id1, id2, id3);
+      assert.deepStrictEqual(p.toString(),`${id1}/${id2}/${id3}`);
     });
   });
 });
