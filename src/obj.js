3fee5436d757bdb82f947b699661f8bef41936ea
Hideyuki MORITA <emeitch@emeitch.com>
2017-07-06 00:22:02 +0900

    Obj#getとPath#reduceの実装が被っていたので統一化

--- src/obj.js
+++ src/obj.js
@@ -1,14 +1,17 @@
 import Env from './env';
 import UUID from './uuid';
+import Path from './path';
 
 export default class Obj extends Env {
   get(key) {
-    const log = this.store.activeLog(this.id, key);
-    if (!log) {
+    const path = new Path(this.id, key);
+    const v = path.reduce(this);
+    if (v instanceof UUID) {
+      return new Obj(this.store, v);
+    } else if (v === path){
       return undefined;
+    } else {
+      return v;
     }
-
-    const v = log.val.reduce(this);
-    return v instanceof UUID ? new Obj(this.store, v) : v;
   }
 }
