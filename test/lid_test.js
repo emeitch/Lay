9522ce9eb32696a045aff3c8fd0125d7c0700256
Hideyuki MORITA <emeitch@emeitch.com>
2017-07-10 21:22:15 +0900

    LIDテストの記述を是正

--- test/lid_test.js
+++ test/lid_test.js
@@ -5,20 +5,20 @@ import LID from '../src/lid';
 describe("LID", () => {
   describe("#toString", () => {
     context("without origin", () => {
-      it("should return blank node style string", () => {
+      it("should return a blank node style string", () => {
         assert(new LID().toString().match(/^_:.*$/));
       });
     });
 
     context("with origin", () => {
-      it("should return blank node style string with origin", () => {
+      it("should return a blank node style string with origin", () => {
         assert(new LID("origin").toString().match(/^_:origin$/));
       });
     });
   });
 
   describe("#toJSON", () => {
-    it("should return same #toString", () => {
+    it("should return a string which is same #toString", () => {
       const lid = new LID();
       assert(lid.toJSON() === lid.toString());
     });
