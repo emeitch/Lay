27c4e9606724e39c887958b5655aaeb1e4f64b3b
Hideyuki MORITA <emeitch@emeitch.com>
2017-07-28 22:44:55 +0900

    Actをチェーンできるようにした

--- src/act.js
+++ src/act.js
@@ -2,6 +2,21 @@ import Val from './val';
 
 export default class Act extends Val {
   run() {
-    this.origin();
+    if (this.prev) {
+      const promise = this.prev.run();
+      promise.then(this.origin);
+      return promise;
+    } else {
+      const promise = new Promise((resolve, _) => {
+        this.origin();
+        resolve();
+      });
+      return promise;
+    }
+  }
+
+  and(act) {
+    act.prev = this;
+    return act;
   }
 }
--- test/act_test.js
+++ test/act_test.js
@@ -3,19 +3,36 @@ import assert from 'assert';
 import Act from '../src/act';
 
 describe("Act", () => {
-
   describe("#run", () => {
     let act;
     let executed = false;
     before(() => {
-      act = new Act(() => {
-        executed = true;
-      });
+      act = new Act(() => { executed = true; });
     });
 
     it("should execute the act", () => {
-      act.run();
+      assert(executed === false);
+      act.run(); // sync
       assert(executed === true);
+    });
+  });
+
+  describe("#and", () => {
+    it("should execute first and second acts", () => {
+      let firstFinished = false;
+      const first = new Act(() => { firstFinished = true; });
+
+      let secondFinished = false;
+      const second = new Act(() => { secondFinished = true; });
+
+      let thirdFinished = false;
+      const third = new Act(() => { thirdFinished = true; });
+
+      return first.and(second).and(third).run().then(() => {
+        assert(firstFinished === true);
+        assert(secondFinished === true);
+        assert(thirdFinished === true);
+      });
     });
   });
 });
