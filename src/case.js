9f107ec5a5ab0f2c864192c47c7f0a291b7d0d45
Hideyuki MORITA <emeitch@emeitch.com>
2017-08-03 00:01:09 +0900

    Case用にAltという構造を入れてパターンと評価式をペアで管理できるようにした

--- src/case.js
+++ src/case.js
@@ -1,18 +1,28 @@
 import Exp from './exp';
 import Box from './box';
 
+class CaseAlt {
+  constructor(pat, exp) {
+    this.pat = pat;
+    this.exp = exp;
+  }
+}
+export function alt(pat, exp) {
+  return new CaseAlt(pat, exp);
+}
+
 export default class Case extends Exp {
-  constructor(exp, ...patternAndExpPairs) {
+  constructor(exp, ...alts) {
     super();
     this.exp = exp;
-    this.patternAndExpPairs = patternAndExpPairs;
+    this.alts = alts;
   }
 
   reduce(env=new Box()) {
     const val = this.exp.reduce(env);
-    const [pattern, matchedExp] = this.patternAndExpPairs;
-    if (val.match(pattern)) {
-      return matchedExp.reduce(env);
+    const alt = this.alts[0];
+    if (val.match(alt.pat)) {
+      return alt.exp.reduce(env);
     }
 
     throw "matched pattern not found";
--- test/case_test.js
+++ test/case_test.js
@@ -2,13 +2,15 @@ import assert from 'assert';
 
 import { v } from '../src/val';
 import Box from '../src/box';
-import Case from '../src/case';
+import Case, { alt } from '../src/case';
 
 describe("Case", () => {
   describe("#reduce", () => {
     context("unmatching", () => {
       it("should reduce the case", () => {
-        const kase = new Case(v(1), v(0), v("result"));
+        const kase = new Case(v(1),
+          alt(v(0), v("result"))
+        );
         const env = new Box();
         assert.throws(() => kase.reduce(env), /matched pattern not found/);
       });
@@ -16,7 +18,9 @@ describe("Case", () => {
 
     context("matching val pattern", () => {
       it("should reduce the matched result exp", () => {
-        const kase = new Case(v(1), v(1), v("result"));
+        const kase = new Case(v(1),
+          alt(v(1), v("result"))
+        );
         assert.deepStrictEqual(kase.reduce(), v("result"));
       });
     });
