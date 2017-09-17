a01ff192ab0c04c229e6641aa5a2964ce68ad487
Hideyuki MORITA <emeitch@emeitch.com>
2017-09-18 07:35:52 +0900

    派生オブジェクトの実apply部分が簡単に書けるように修正

--- src/func.js
+++ src/func.js
@@ -6,18 +6,21 @@ export default class Func {
     this.args = args;
   }
 
-  partialApply(...args) {
+  get arity() {
+    return this.base.arity;
+  }
+
+  apply(...args) {
     const allArgs = this.args.concat(args);
     if (allArgs.length < this.arity) {
       return new Func(this, allArgs);
     }
 
-    return undefined;
+    return this._apply(...allArgs);
   }
 
-  apply(...args) {
-    const allArgs = this.args.concat(args);
-    return this.base.apply(...allArgs);
+  _apply(...args) {
+    return this.base.apply(...args);
   }
 }
 
@@ -26,12 +29,7 @@ export class Plus extends Func {
     return 2;
   }
 
-  apply(...args) {
-    const func = this.partialApply(...args);
-    if (func) {
-      return func;
-    }
-
+  _apply(...args) {
     const [fst, snd] = args;
     return new Val(fst.origin + snd.origin);
   }
