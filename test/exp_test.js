59bcf46c27ab14266898f321dbd5f6a9eb5ec55b
Hideyuki MORITA <emeitch@emeitch.com>
2017-10-09 21:44:18 +0900

    expに入れていたfunc系テストをfuncのテストに移植

--- test/exp_test.js
+++ test/exp_test.js
@@ -1,11 +1,10 @@
 import assert from 'assert';
 
 import { v } from '../src/val';
-import Func, { func, plus } from '../src/func';
+import { plus } from '../src/func';
 import UUID from '../src/uuid';
 import Path from '../src/path';
 import Exp, { exp } from '../src/exp';
-import { kase, alt, grd, otherwise } from '../src/case';
 import Book from '../src/book';
 
 describe("Exp", () => {
@@ -37,110 +36,6 @@ describe("Exp", () => {
       it("should reduce the expression", () => {
         const e = exp((x, y) => x * y, v(2), v(3));
         assert.deepStrictEqual(e.reduce(), v(6));
-      });
-    });
-
-    context("partial evaluation", () => {
-      it("should reduce the expression", () => {
-        const e = exp(plus, v(2));
-
-        const reduced = e.reduce();
-        assert(reduced instanceof Func);
-
-        const e2 = exp(reduced, v(3));
-        assert.deepStrictEqual(e2.reduce(), v(5));
-      });
-    });
-
-    context("func literal expression", () => {
-      it("should reduce the expression", () => {
-        const e = exp(
-          func("x", "y", exp(plus, "x", "y")),
-          v(2),
-          v(3)
-        );
-        assert.deepStrictEqual(e.reduce(), v(5));
-      });
-    });
-
-    context("partial reducing func", () => {
-      it("should reduce the expression", () => {
-        const e = exp(
-          exp(
-            func("x", "y", exp(plus, "x", "y")),
-            v(2)
-          ),
-          v(3)
-        );
-        assert.deepStrictEqual(e.reduce(), v(5));
-      });
-    });
-
-    context("defined function", () => {
-      it("should reduce the expression", () => {
-        const book = new Book();
-        book.assign("f", func("y", exp(plus, v(3), "y")));
-        
-        assert.deepStrictEqual(exp("f", v(2)).reduce(book), v(5));
-      });
-    });
-
-    context("recursive function", () => {
-      it("should reduce the expression", () => {
-        const book = new Book();
-        book.assign("f", func(
-          "x",
-          exp(
-            kase(
-              alt(
-                "y",
-                [
-                  grd(
-                    exp(x => x == 0, "y"),
-                    "y"
-                  ),
-                  grd(
-                    otherwise,
-                    exp(
-                      plus,
-                      v(2),
-                      exp(
-                        "f",
-                        exp(
-                          plus,
-                          "y",
-                          v(-1)
-                        )
-                      )
-                    )
-                  )
-                ]
-              )
-            ),
-            "x"
-          )
-        ));
-
-        assert.deepStrictEqual(exp("f", v(4)).reduce(book), v(8));
-      });
-    });
-
-    context("currying function", () => {
-      it("should reduce the expression", () => {
-        const e = exp(
-          exp(
-            func(
-              "x",
-              func(
-                "y",
-                exp(plus, "x", "y")
-              )
-            ),
-            v(2)
-          ),
-          v(3)
-        );
-        assert.deepStrictEqual(e.reduce(), v(5));
       });
     });
   });
--- test/func_test.js
+++ test/func_test.js
@@ -2,10 +2,116 @@ import assert from 'assert';
 
 import { v } from '../src/val';
 import { exp } from '../src/exp';
-import Func, { func } from '../src/func';
+import Book from '../src/book';
+import { kase, alt, grd, otherwise } from '../src/case';
+import Func, { func, plus } from '../src/func';
 
 describe("Func", () => {
-  describe("create", () => {
+  describe("#apply", () => {
+    context("func literal expression", () => {
+      it("should reduce the expression", () => {
+        const e = exp(
+          func("x", "y", exp(plus, "x", "y")),
+          v(2),
+          v(3)
+        );
+        assert.deepStrictEqual(e.reduce(), v(5));
+      });
+    });
+
+    context("partial reduction for func literal expression", () => {
+      it("should reduce the expression", () => {
+        const e = exp(
+          exp(
+            func("x", "y", exp(plus, "x", "y")),
+            v(2)
+          ),
+          v(3)
+        );
+        assert.deepStrictEqual(e.reduce(), v(5));
+      });
+    });
+
+    context("partial reduction", () => {
+      it("should reduce the expression", () => {
+        const e = exp(plus, v(2));
+
+        const reduced = e.reduce();
+        assert(reduced instanceof Func);
+
+        const e2 = exp(reduced, v(3));
+        assert.deepStrictEqual(e2.reduce(), v(5));
+      });
+    });
+
+    context("defined function", () => {
+      it("should reduce the expression", () => {
+        const book = new Book();
+        book.assign("f", func("y", exp(plus, v(3), "y")));
+
+        assert.deepStrictEqual(exp("f", v(2)).reduce(book), v(5));
+      });
+    });
+
+    context("recursive function", () => {
+      it("should reduce the expression", () => {
+        const book = new Book();
+        book.assign("f", func(
+          "x",
+          exp(
+            kase(
+              alt(
+                "y",
+                [
+                  grd(
+                    exp(x => x == 0, "y"),
+                    "y"
+                  ),
+                  grd(
+                    otherwise,
+                    exp(
+                      plus,
+                      v(2),
+                      exp(
+                        "f",
+                        exp(
+                          plus,
+                          "y",
+                          v(-1)
+                        )
+                      )
+                    )
+                  )
+                ]
+              )
+            ),
+            "x"
+          )
+        ));
+
+        assert.deepStrictEqual(exp("f", v(4)).reduce(book), v(8));
+      });
+    });
+
+    context("currying function", () => {
+      it("should reduce the expression", () => {
+        const e = exp(
+          exp(
+            func(
+              "x",
+              func(
+                "y",
+                exp(plus, "x", "y")
+              )
+            ),
+            v(2)
+          ),
+          v(3)
+        );
+        assert.deepStrictEqual(e.reduce(), v(5));
+      });
+    });
+
     context("native function exp", () => {
       it("should reduce the expression", () => {
         const e = exp(
