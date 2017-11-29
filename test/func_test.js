5c7b62b149809d2d8b40da7509320ecbd1353c62
Hideyuki MORITA <emeitch@emeitch.com>
2017-11-30 01:43:32 +0900

    引数無しの関数も問題なく簡約できることをテスト

--- test/func_test.js
+++ test/func_test.js
@@ -232,5 +232,12 @@ describe("Func", () => {
         assert.deepStrictEqual(e.reduce(book), v("else"));
       });
     });
+
+    context("arity zero func", () => {
+      it("should return the reduced value", () => {
+        assert.deepStrictEqual(exp(func(exp(plus, v(1), v(2)))).reduce(), v(3));
+        assert.deepStrictEqual(exp(func(v(3))).reduce(), v(3));
+      });
+    });
   });
 });
