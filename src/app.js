8c93ca4df86fdf33d7394acdfa949f25660877ad
Hideyuki MORITA <emeitch@emeitch.com>
2017-12-30 16:37:57 +0900

    tasksに適当にtaskを追加した状態を表現してみた

--- src/app.js
+++ src/app.js
@@ -1,8 +1,41 @@
 /* eslint-env browser */
 import Book from './book';
+import Obj from './obj';
+import { uuid } from './uuid';
+import v from './v';
 
-const domain = new Book();
+const d = new Book();
+d.assign("tasks", uuid());
+d.assign("item", uuid());
+d.assign("title", uuid());
 
-console.log(domain);
+{
+  const t = uuid();
+  d.put(d.resolve("tasks"), d.resolve("item"), t);
+  d.put(t, d.resolve("title"), v("buy the milk"));
+
+  const to = new Obj(d, t);
+  console.log(to.get(d.resolve("title")));
+}
+
+{
+  const t = uuid();
+  d.put(d.resolve("tasks"), d.resolve("item"), t);
+  d.put(t, d.resolve("title"), v("buy the beer"));
+
+  const to = new Obj(d, t);
+  console.log(to.get(d.resolve("title")));
+}
+
+{
+  const t = uuid();
+  d.put(d.resolve("tasks"), d.resolve("item"), t);
+  d.put(t, d.resolve("title"), v("buy the wine"));
+
+  const to = new Obj(d, t);
+  console.log(to.get(d.resolve("title")));
+}
+
+console.log(d);
 
 console.log("Lay: Hello, world!");
