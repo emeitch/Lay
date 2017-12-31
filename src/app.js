ea478d7df3957922518761f5b4c85b528ee5f711
Hideyuki MORITA <emeitch@emeitch.com>
2018-01-01 00:57:48 +0900

    Book#objsを利用してapp.jsでオブジェクトを取得表示するようにした

--- src/app.js
+++ src/app.js
@@ -2,25 +2,17 @@
 import Book from './book';
 
 const d = new Book();
-
 {
   const to = d.new();
   to.set("title", "buy the milk");
-  console.log(to.get("title"));
 }
-
 {
   const to = d.new();
   to.set("title", "buy the beer");
-  console.log(to.get("title"));
 }
-
 {
   const to = d.new();
   to.set("title", "buy the wine");
-  console.log(to.get("title"));
 }
 
-console.log(d);
-
-console.log("Lay: Hello, world!");
+d.objs().forEach(o => console.log(o.get("title")));
