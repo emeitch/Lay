527a296047a0f93ab527e6d29d06f78a5a7cc878
Hideyuki MORITA <emeitch@emeitch.com>
2018-01-01 02:23:38 +0900

    よりUI操作したような流れに変更

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@
 import Book from './book';
 
 const d = new Book();
-d.new({title: "buy the milk"});
-d.new({title: "buy the beer"});
-d.new({title: "buy the wine"});
+
+d.new().set("title", "buy the milk");
+
+d.new().set("title", "buy the beer");
+
+d.new().set("title", "buy the wine");
 
 d.objs().forEach(o => console.log(o.get("title")));
