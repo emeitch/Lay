ed7cfe3a6ea386dbfa9c1b1a945a6cf06d8808dd
Hideyuki MORITA <emeitch@emeitch.com>
2018-01-02 11:43:04 +0900

    app.jsで型指定を追加

--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 /* eslint-env browser */
 import Book from './book';
+import { sym } from './sym';
 
 const d = new Book();
 
-d.new().set("title", "buy the milk");
+d.new()
+  .set("type", sym("Task"))
+  .set("title", "buy the milk");
+d.new()
+  .set("type", sym("Task"))
+  .set("title", "buy the beer");
+d.new()
+  .set("type", sym("Task"))
+  .set("title", "buy the wine");
 
-d.new().set("title", "buy the beer");
-
-d.new().set("title", "buy the wine");
-
-d.objs().forEach(o => console.log(o.get("title")));
+d.objs().forEach(o => {
+  console.log(o.get("type"));
+  console.log(o.get("title"));
+});
