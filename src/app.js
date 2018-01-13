7e90c1b83ea9ed3fa4ca7b99840b109f8ba7ec35
Hideyuki MORITA <emeitch@emeitch.com>
2018-01-13 16:29:41 +0900

    プロトタイプを遅延定義してみた

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 /* eslint-env browser */
 import Book from './book';
 import { sym } from './sym';
+import { path } from './path';
+import { plus } from './func';
+import { exp } from './exp';
 import v from './v';
 
 const d = new Book();
@@ -33,5 +36,19 @@ d.objs().forEach(o => {
   o.keys().forEach(k => {
     console.log(k, o.get(k));
   });
+  console.log("----------");
+});
+
+d.new();
+{
+  const taskClass = d.objs().pop();
+  taskClass.send(v("set"), v("title_desc"),
+    exp(plus, path(sym("self"), v("title")), v(" description")));
+
+  d.set("Task", taskClass.id);
+}
+
+d.objs().forEach(o => {
+  console.log("title_desc", o.get(v("title_desc")));
   console.log("----------");
 });
