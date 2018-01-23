99fafa48e2af2109d2e9d5fb9c97ea5ea9cecce1
Hideyuki MORITA <emeitch@emeitch.com>
2018-01-24 00:46:55 +0900

    定義時に値が評価されないように無理やりset2を組み込みつつcompleteを定義

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 import Book from './book';
 import { sym } from './sym';
 import { path } from './path';
-import { plus } from './func';
+// import { plus } from './func';
 import { exp } from './exp';
 import v from './v';
 
@@ -14,6 +14,7 @@ d.new();
   o.send(v("set"), v("tag"), sym("Task"));
   o.send(v("set"), v("title"), v("buy the milk"));
   o.send(v("set"), v("state"), sym("active"));
+  o.send(v("set"), v("set2"), path(sym("self"), v("set")));
 }
 
 d.new();
@@ -22,6 +23,7 @@ d.new();
   o.send(v("set"), v("tag"), sym("Task"));
   o.send(v("set"), v("title"), v("buy the beer"));
   o.send(v("set"), v("state"), sym("active"));
+  o.send(v("set"), v("set2"), path(sym("self"), v("set")));
 }
 
 d.new();
@@ -30,6 +32,7 @@ d.new();
   o.send(v("set"), v("tag"), sym("Task"));
   o.send(v("set"), v("title"), v("buy the wine"));
   o.send(v("set"), v("state"), sym("active"));
+  o.send(v("set"), v("set2"), path(sym("self"), v("set")));
 }
 
 d.objs().forEach(o => {
@@ -43,15 +46,19 @@ d.objs().forEach(o => {
 d.new();
 {
   const taskClass = d.objs().pop();
-  taskClass.send(v("set"), v("title_desc"),
-    exp(plus, path(sym("self"), v("title")), v(" description")));
+  taskClass.send(v("set"), v("complete"),
+    exp(path(sym("self"), v("set2")), v("state"), sym("completed")));
 
   d.set("Task", taskClass.id);
 }
 
 d.objs().forEach(o => {
-  const k = v("title_desc");
+  o.send(v("complete"));
+
+  const k = v("state");
   const val = o.get(k);
-  console.log(k.stringify(), ":", val.stringify());
-  console.log("----------");
+  if (val) {
+    console.log(k.stringify(), ":", val.stringify());
+    console.log("----------");
+  }
 });
