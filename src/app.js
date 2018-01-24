7d2c89ba8a24d4b5c1fbd29e09329fc5a294fc13
Hideyuki MORITA <emeitch@emeitch.com>
2018-01-25 00:22:19 +0900

    Obj#setを活用して引数をreduceせずプロパティ定義ができるようにした

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import Book from './book';
 import { sym } from './sym';
 import { path } from './path';
 // import { plus } from './func';
-import { exp } from './exp';
 import v from './v';
 
 const d = new Book();
@@ -11,28 +10,25 @@ const d = new Book();
 d.new();
 {
   const o = d.objs().pop();
-  o.send(v("set"), v("tag"), sym("Task"));
-  o.send(v("set"), v("title"), v("buy the milk"));
-  o.send(v("set"), v("state"), sym("active"));
-  o.send(v("set"), v("set2"), path(sym("self"), v("set")));
+  o.set(v("tag"), sym("Task"));
+  o.set(v("title"), v("buy the milk"));
+  o.set(v("state"), sym("active"));
 }
 
 d.new();
 {
   const o = d.objs().pop();
-  o.send(v("set"), v("tag"), sym("Task"));
-  o.send(v("set"), v("title"), v("buy the beer"));
-  o.send(v("set"), v("state"), sym("active"));
-  o.send(v("set"), v("set2"), path(sym("self"), v("set")));
+  o.set(v("tag"), sym("Task"));
+  o.set(v("title"), v("buy the beer"));
+  o.set(v("state"), sym("active"));
 }
 
 d.new();
 {
   const o = d.objs().pop();
-  o.send(v("set"), v("tag"), sym("Task"));
-  o.send(v("set"), v("title"), v("buy the wine"));
-  o.send(v("set"), v("state"), sym("active"));
-  o.send(v("set"), v("set2"), path(sym("self"), v("set")));
+  o.set(v("tag"), sym("Task"));
+  o.set(v("title"), v("buy the wine"));
+  o.set(v("state"), sym("active"));
 }
 
 d.objs().forEach(o => {
@@ -46,13 +42,19 @@ d.objs().forEach(o => {
 d.new();
 {
   const taskClass = d.objs().pop();
-  taskClass.send(v("set"), v("complete"),
-    exp(path(sym("self"), v("set2")), v("state"), sym("completed")));
+  taskClass.set(
+    v("complete"),
+    path(sym("self"), [v("set"), v("state"), sym("completed")])
+  );
 
   d.set("Task", taskClass.id);
 }
 
 d.objs().forEach(o => {
+  if (d.get("Task") === o.id) {
+    return;
+  }
+
   o.send(v("complete"));
 
   const k = v("state");
