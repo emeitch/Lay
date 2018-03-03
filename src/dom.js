7c5e3c0ca1419e0b6fae2e453feabf8de468804f
Hideyuki MORITA <emeitch@emeitch.com>
2018-03-03 21:44:05 +0900

    elm関数やe関数をdom.js内で定義

--- src/dom.js
+++ src/dom.js
@@ -57,3 +57,152 @@ export default class DOM {
     );
   }
 }
+
+function n(head, origin) {
+  if (Array.isArray(origin)) {
+    return path("Array", ["new", head].concat(origin));
+  } else {
+    const o = Object.keys(origin).reduce((r, k) => r.concat([k, origin[k]]), []);
+    return path("Map", ["new", head].concat(o));
+  }
+}
+
+export function elm(head, ...children) {
+  let attr = {};
+  if (children[0].constructor === Object) {
+    attr = children.shift();
+  }
+
+  if (children && children.length > 0) {
+    Object.assign(attr, {children: n("children", children)});
+  }
+
+  return n(head, attr);
+}
+
+export const e = {};
+const etags = [
+  "a",
+  "abbr",
+  "acronym",
+  "address",
+  "applet",
+  "area",
+  "article",
+  "aside",
+  "audio",
+  "b",
+  "base",
+  "basefont",
+  "bdi",
+  "bdo",
+  "big",
+  "blockquote",
+  "body",
+  "br",
+  "button",
+  "canvas",
+  "caption",
+  "center",
+  "cite",
+  "code",
+  "col",
+  "colgroup",
+  "datalist",
+  "dd",
+  "del",
+  "details",
+  "dfn",
+  "dialog",
+  "dir",
+  "div",
+  "dl",
+  "dt",
+  "em",
+  "embed",
+  "fieldset",
+  "figcaption",
+  "figure",
+  "font",
+  "footer",
+  "form",
+  "frame",
+  "frameset",
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "head",
+  "header",
+  "hr",
+  "html",
+  "i",
+  "iframe",
+  "img",
+  "input",
+  "ins",
+  "kbd",
+  "keygen",
+  "label",
+  "legend",
+  "li",
+  "link",
+  "main",
+  "map",
+  "mark",
+  "menu",
+  "menuitem",
+  "meta",
+  "meter",
+  "nav",
+  "noframes",
+  "noscript",
+  "object",
+  "ol",
+  "optgroup",
+  "option",
+  "output",
+  "p",
+  "param",
+  "picture",
+  "pre",
+  "progress",
+  "q",
+  "rp",
+  "rt",
+  "ruby",
+  "s",
+  "samp",
+  "script",
+  "section",
+  "select",
+  "small",
+  "source",
+  "span",
+  "strike",
+  "strong",
+  "style",
+  "sub",
+  "summary",
+  "sup",
+  "table",
+  "tbody",
+  "td",
+  "textarea",
+  "tfoot",
+  "th",
+  "thead",
+  "time",
+  "title",
+  "tr",
+  "track",
+  "tt",
+  "u",
+  "ul",
+  "var",
+  "video",
+  "wbr"
+];
+etags.forEach(etag => {
+  e[etag] = (...args) => elm(etag, ...args);
+});
