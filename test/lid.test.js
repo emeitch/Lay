14470f5d68b2b0124673e0b1059b887153efe5d4
MORITA Hideyuki [エムエイチ] <emeitch@emeitch.com>
2018-08-17 00:01:04 +0900

    LIDのテストコードをひとまず追加

--- test/lid.test.js
+++ test/lid.test.js
@@ -1,1 +1,18 @@
 import assert from 'assert';
+
+import LID from '../src/lid';
+
+describe("LID", () => {
+  describe("#stringify", () => {
+    it("should return uuid urn", () => {
+      assert(new LID().stringify().match(/^urn:uuid:.*$/));
+    });
+  });
+
+  describe("#reducible", () => {
+    it("should return true by default behavior", () => {
+      const lid = new LID();
+      assert.deepStrictEqual(lid.reducible, false);
+    });
+  });
+});
