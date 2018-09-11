8c9b09bd98e8182e8117e651423cb6f23da5d1a4
Hideyuki MORITA <emeitch@emeitch.com>
2018-09-12 00:15:57 +0900

    Edgeを追加

---
+++ src/edge.js
@@ -0,0 +1,7 @@
+export default class Log {
+  constructor(tail, label, head) {
+    this.tail = tail;
+    this.label = label;
+    this.head = head;
+  }
+}
---
+++ test/edge.test.js
@@ -0,0 +1,18 @@
+import assert from 'assert';
+
+import Edge from '../src/edge';
+
+describe("Edge", () => {
+  describe("#constructor", () => {
+    it("should return a Edge object", () => {
+      const tail = {};
+      const label = "label1";
+      const head = {};
+      const e = new Edge(tail, label, head);
+
+      assert(e.tail === tail);
+      assert(e.label === label);
+      assert(e.head === head);
+    });
+  });
+});
