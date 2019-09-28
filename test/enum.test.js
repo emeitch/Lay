445477255bd63cf13a5cc25649554f06d791ef72
Hideyuki MORITA <emeitch@emeitch.com>
2019-09-29 01:07:53 +0900

    _onPutByProtoの解説を追加

--- test/enum.test.js
+++ test/enum.test.js
@@ -38,6 +38,7 @@ describe("Enum", () => {
   context("implicit _proto specifing", () => {
     describe("define child objects", () => {
       it("should define enum values", () => {
+        // add _proto for no _proto Obj children by Enum#__onPutByProto
         store.put({
           _proto: "Enum",
           _id: "Foo",
